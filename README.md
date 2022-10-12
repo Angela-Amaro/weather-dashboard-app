0a3df54428781a6bc53f5b1b411ce7be6c4fa677
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-12 01:10:08 -0500

    finished website, changed colors for a more easier read, created local storage, created past searches bank.

--- README.md
+++ README.md
@@ -1,49 +1,29 @@
-GIVEN a weather dashboard with form inputs
-
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-WHEN I view current weather conditions for that city
-
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-
-WHEN I view future weather conditions for that city
-
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-WHEN I click on a city in the search history
-
-THEN I am again presented with current and future conditions for that city
 # Angela's weather-dashboard-app
 
 ## Description
 
-Hello and welcome to my password generator application! Here you can generate a safe and secure password based on your criteria. In this application you can have a password generated between 8-129 characters long! you can generate a password with numbers, uppercase letters, lowercase letters, and special characters.
+Hello and welcome to my weather dashboard. Input any city you want to see the current weather and a 5 day forecast! You can also see your past searches as well as re-search them by clicking on them in the past searches bank.
 
 ## Snapshot
- 
+
 [Active website link](https://angela-amaro.github.io/password-generator/)
 
+![Website gif](./weatherrecord.gif)
 
-![Website gif](./Assets/gif_of_pass_generator.gif)
+## process
 
-
-
-## process 
-
-I was given starter code for this password generator but none of the functions had code inside. In this generator I created variables, loops, arrays and methods to make this generator function properly to the users input to generate a safe and secure password.
+I built this weather dashboard from scratch utilizing a W3 styling within my HTML. I then added functionality to my website using Java script and untilizing a weather API to gather live data.
 
 ## languages
 
-HTML/CSS/JS
+HTML/W3 styling/JS
 
 ## author
 
 Angela Amaro
 
-## Contact 
+## Contact
 
 [My GitHub](https://github.com/Angela-Amaro)
 
 [My Linkedin](https://www.linkedin.com/in/angela-amaro-342792204/)
-
--- script.js
+++ script.js
@@ -5,14 +5,45 @@ var cityNameEl = $("#citynamedate");
 var cityTempEl = $("#maintemp");
 var cityWindEl = $("#mainwind");
 var cityHumidEl = $("#mainhumid");
+
 var forecast0dateEl = $("#date0");
 var forecast0tempEl = $("#temp0");
-var forcast0humidEl
+var forcast0humidEl = $("#humid0");
+var forcast0windEl = $("#wind0");
 
+var forecast1dateEl = $("#date1");
+var forecast1tempEl = $("#temp1");
+var forcast1humidEl = $("#humid1");
+var forcast1windEl = $("#wind1");
 
-function search(event) {
-  event.preventDefault();
-  var city = inputEl.val();
+var forecast2dateEl = $("#date2");
+var forecast2tempEl = $("#temp2");
+var forcast2humidEl = $("#humid2");
+var forcast2windEl = $("#wind2");
+
+var forecast3dateEl = $("#date3");
+var forecast3tempEl = $("#temp3");
+var forcast3humidEl = $("#humid3");
+var forcast3windEl = $("#wind3");
+
+var forecast4dateEl = $("#date4");
+var forecast4tempEl = $("#temp4");
+var forcast4humidEl = $("#humid4");
+var forcast4windEl = $("#wind4");
+
+var pastSearchesEl = $('#past')
+var cityHistArray = []
+
+if (localStorage.getItem("cityHistory")) {
+    cityHistArray = JSON.parse(localStorage.getItem("cityHistory"))
+
+    for (i = 0; i <cityHistArray.length; i++) {
+        console.log(cityHistArray)
+        addSearchBtn(cityHistArray[i])
+    }
+}
+
+function search(city) {
   var latLongFetchUrl =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
@@ -27,23 +58,28 @@ function search(event) {
         lat = data[0].lat;
         lon = data[0].lon;
 
-        var dailyFetchUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=b110023a32982697701df76e58c95ed4'
-
+        var dailyFetchUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=b110023a32982697701df76e58c95ed4'
+        var forecastFetchUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=b110023a32982697701df76e58c95ed4'
         //now that we have a city name, lat, and lon we can get the details
         fetch(dailyFetchUrl)
             .then(function (response) {
                 return response.json();
             }).then(function (data) {
 
-                // addSearchBtn(data.city.name);
+                if (!cityHistArray.includes(data.name)) {
+                    cityHistArray.push(data.name)
+                    localStorage.setItem("cityHistory", JSON.stringify(cityHistArray))
+                    cityHistArray = JSON.parse(localStorage.getItem("cityHistory"))
+                    addSearchBtn(data.name);
+                }
                 console.log(data)
-                var displayTime = moment.unix(data.list[0].dt).format("L");
+                var displayTime = moment.unix(data.dt).format("L");
                 var currentDate = $("<span>", { "class": "card-title" })
                 currentDate.text(" (" + displayTime + ") ");
-                cityNameEl.text(data.city.name);
+                cityNameEl.text(data.name);
 
                 // cityNameEl.append(currentDate);
-                var iconCode = data.list[0].weather[0].icon;
+                var iconCode = data.weather[0].icon;
                 var dailyUrl = "https://openweathermap.org/img/w/" + iconCode + ".png"
 
                 var dayIcon = $("<img>", { "src": dailyUrl });
@@ -51,18 +87,56 @@ function search(event) {
 
                 // main display icon, temp, wind speed, and humidity
                 cityNameEl.append(currentDate).append(dayIcon);
-                cityTempEl.text(data.list[0].main.temp + " F");
-                cityWindEl.text(data.list[0].wind.speed + " MPH");
-                cityHumidEl.text(data.list[0].main.humidity + " %");
+                cityTempEl.text("Temperature: " + data.main.temp + " F");
+                cityWindEl.text("Wind speed: " + data.wind.speed + " MPH");
+                cityHumidEl.text("Humidity: " + data.main.humidity + " %");
+                fetch(forecastFetchUrl)
+                .then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    console.log(data)
+                    var index = 7;
+                    for (i = 0; i < 5 ; i++){
+                        var date = $("#date" + i);
+                        var temp = $("#temp" + i);
+                        var humid = $("#humid" + i);
+                        var wind = $("#wind" + i);
+                        populateCard(date, temp, humid, wind, data, index)
+                        index += 8
+                    }
             })
         })
-    }
+    })
+}
 
 
-fetchButton.on("click", search);
+fetchButton.on("click", function(event) {
+    event.preventDefault
+    search(inputEl.val())});
 
-// function results() {
-//     var tableResults = ""
-//     fetch(tableResults)
-//         .then(function ())
-//
+function populateCard(dateEl, tempEl, humidEl, windEl, data, index) {
+    var object = data.list[index]
+    dateEl.text(moment.unix(object.dt).format("L"))
+    var iconCode = object.weather[0].icon;
+    var dailyUrl = "https://openweathermap.org/img/w/" + iconCode + ".png"
+
+    var dayIcon = $("<img>", { "src": dailyUrl });
+    dayIcon.attr("lt", "Weather Icon");
+
+
+    dateEl.append(dayIcon);
+    tempEl.text(" Temperature: " + object.main.temp + " F");
+    windEl.text(" Wind speed: " + object.wind.speed + " MPH");
+    humidEl.text(" Humidity: " + object.main.humidity + " %");
+}
+
+function addSearchBtn(cityName) {
+        var buttonEl = $("<button>", {"class":"w3-btn"})
+        buttonEl.text(cityName)
+        pastSearchesEl.prepend(buttonEl)
+}
+
+pastSearchesEl.on("click", "button", function() {
+   search($(this).text()) 
+})
+
